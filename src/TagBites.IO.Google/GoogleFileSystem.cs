92cce012696bf7f705d969b125d15604adc20875
Bartosz Jędrzejak <bartosz.jedrzejak@gmail.com>
2026-07-09 20:43:36 +0200

    docs: add missing XML doc comments

--- src/TagBites.IO.Google/GoogleFileSystem.cs
+++ src/TagBites.IO.Google/GoogleFileSystem.cs
@@ -2,8 +2,17 @@ using Google.Apis.Auth.OAuth2;
 
 namespace TagBites.IO.Google;
 
+/// <summary>
+/// Exposes static method for creating a Google Cloud Storage file system.
+/// </summary>
 public class GoogleFileSystem
 {
+    /// <summary>
+    /// Creates a Google Cloud Storage file system.
+    /// </summary>
+    /// <param name="bucketName">The name of an existing Google Cloud Storage bucket.</param>
+    /// <param name="jsonCredential">The JSON key content of a Google Cloud service account with access to the bucket.</param>
+    /// <returns>A Google Cloud Storage file system contains the procedures that are used to perform file and directory operations.</returns>
     public static FileSystem Create(string bucketName, string jsonCredential)
     {
         var credential = CredentialFactory.FromJson<ServiceAccountCredential>(jsonCredential).ToGoogleCredential();
