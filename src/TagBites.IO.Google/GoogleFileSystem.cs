6c561acf6525726a604ac748c4bb1fb8f68e917f
Bartosz Jędrzejak <bartosz.jedrzejak@gmail.com>
2026-07-31 10:05:33 +0200

    fix: validate arguments before parsing the credential

--- src/TagBites.IO.Google/GoogleFileSystem.cs
+++ src/TagBites.IO.Google/GoogleFileSystem.cs
@@ -13,8 +13,14 @@ public class GoogleFileSystem
     /// <param name="bucketName">The name of an existing Google Cloud Storage bucket.</param>
     /// <param name="jsonCredential">The JSON key content of a Google Cloud service account with access to the bucket.</param>
     /// <returns>A Google Cloud Storage file system contains the procedures that are used to perform file and directory operations.</returns>
+    /// <exception cref="ArgumentNullException"></exception>
     public static FileSystem Create(string bucketName, string jsonCredential)
     {
+        if (bucketName == null)
+            throw new ArgumentNullException(nameof(bucketName));
+        if (jsonCredential == null)
+            throw new ArgumentNullException(nameof(jsonCredential));
+
         var credential = CredentialFactory.FromJson<ServiceAccountCredential>(jsonCredential).ToGoogleCredential();
         return new FileSystem(new GoogleFileSystemOperations(credential, bucketName));
     }
